832cd4e6271dc05b6f397f099bf164bb118526e2
blu <blu@bluiscool.dev>
2021-03-02 20:24:49 -0600

    Init

--- RustDiscordStatus/RustDiscordStatus/DiscordBot.cs
+++ RustDiscordStatus/RustDiscordStatus/DiscordBot.cs
@@ -1,13 +1,47 @@
+using System;
+using System.Diagnostics.SymbolStore;
+using System.Net;
 using System.Threading.Tasks;
 using DSharpPlus;
+using DSharpPlus.Entities;
+using SourceQuery;
 
 namespace RustDiscordStatus
 {
     public class DiscordBot
     {
-        private readonly string _botToken;
         private DiscordClient _discordClient;
-        
+        private readonly string _botToken;
+        private readonly string _serverIP;
+        private readonly int _threshold;
+        private readonly string _thresholdMessage;
+
+        private bool _ready = false;
+
+        public bool IsReady()
+        {
+            return _ready;
+        }
+
+        public async Task Worker(bool restart)
+        {
+            try
+            {
+                var gs = new GameServer(IPEndPoint.Parse(_serverIP));
+                var playerCount = gs.Players.Count;
+                bool hitThreshold = (playerCount > _threshold);
+                var discordActivity = new DiscordActivity(hitThreshold ? gs.Players.Count + " players!" : _thresholdMessage);
+                discordActivity.ActivityType = ActivityType.Watching;
+                await _discordClient.UpdateStatusAsync(discordActivity, UserStatus.Online);
+            }
+            catch (Exception ex)
+            {
+                var discordActivity = new DiscordActivity(restart ? "Restarting..." : "Offline!");
+                discordActivity.ActivityType = ActivityType.Watching;
+                await _discordClient.UpdateStatusAsync(discordActivity, restart ? UserStatus.Idle : UserStatus.DoNotDisturb);
+            }
+        }
+
         private async Task Startup()
         {
             _discordClient = new DiscordClient(new DiscordConfiguration()
@@ -15,12 +49,21 @@ namespace RustDiscordStatus
                 Token = _botToken,
                 TokenType = TokenType.Bot       
             });
+            
             await _discordClient.ConnectAsync();
+            
+            _discordClient.Ready += async (s, e) =>
+            {
+                _ready = true;
+            };
         }
         
-        public DiscordBot(string botToken)
+        public DiscordBot(string botToken, string serverIp, int threshold, string thresholdMessage)
         {
             _botToken = botToken;
+            _serverIP = serverIp;
+            _threshold = threshold;
+            _thresholdMessage = thresholdMessage;
             Startup().GetAwaiter().GetResult();
         }
     }
--- RustDiscordStatus/RustDiscordStatus/Program.cs
+++ RustDiscordStatus/RustDiscordStatus/Program.cs
@@ -1,14 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
 
 namespace RustDiscordStatus
 {
+    public class Server {
+        public string BotToken { get; set; } 
+        public string ServerIP { get; set; } 
+        public int Threshold { get; set; } 
+        public string ThresholdMessage { get; set; } 
+    }
+
+    public class Config {
+        public int UpdateTime { get; set; } 
+        public bool RestartingOverOffline { get; set; } 
+        public List<Server> Servers { get; set; } 
+    }
+    
     class Program
     {
+        private static Config _config;
+        private static List<DiscordBot> _instances = new List<DiscordBot>();
         static void Main(string[] args)
         {
+            _config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("config.json"));
             Console.WriteLine("Starting up...");
             
-            Console.WriteLine("Hello World!");
+            foreach (var server in _config.Servers)
+            {
+                try
+                {
+                    _instances.Add(new DiscordBot(server.BotToken, server.ServerIP, server.Threshold, server.ThresholdMessage));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to add " + server.ServerIP + "!");
+                }
+            }
+            
+            Console.WriteLine("Loaded data ");
+            while (true)
+            {
+                Thread.Sleep(_config.UpdateTime);
+                foreach (var bot in _instances)
+                    if (bot.IsReady()) bot.Worker(_config.RestartingOverOffline).GetAwaiter().GetResult();
+            }
         }
     }
 }
